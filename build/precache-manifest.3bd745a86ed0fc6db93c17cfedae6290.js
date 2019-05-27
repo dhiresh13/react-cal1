1f3f5eaaa7a84ca7485478f3d7c1593394aa1eb2
dhiresh13 <38584912+dhiresh13@users.noreply.github.com>
2019-05-27 16:29:16 +0530

    react

---
+++ build/precache-manifest.3bd745a86ed0fc6db93c17cfedae6290.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7520677fb1cd8db218c2a7205d1fcbbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a2e76efc17b4d0263af",
+    "url": "/static/css/main.7769ad2f.chunk.css"
+  },
+  {
+    "revision": "b278f877290201dd8c0a",
+    "url": "/static/js/2.6b8457e8.chunk.js"
+  },
+  {
+    "revision": "6a2e76efc17b4d0263af",
+    "url": "/static/js/main.825d331e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e0cf9905177293d4b8243625dc1fec40",
+    "url": "/static/media/4 .e0cf9905.jpg"
+  }
+]);
